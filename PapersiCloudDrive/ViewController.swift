cf2b963e0842fc9d60d62d26f1be8b3039ab1b77
James Valaitis <james.valaitis@springer.com>
2014-07-29 09:53:53 +0100

    Initial Commit

---
+++ PapersiCloudDrive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PapersiCloudDrive
+//
+//  Created by James Valaitis on 29/07/2014.
+//  Copyright (c) 2014 James Valaitis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
